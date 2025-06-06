ddf3159d438b0890b505f2afa3480266e7ffe10e
AlwaysAwake <djssjssl0209@naver.com>
2025-06-06 16:42:02 +0900

    자바 기말 과제

--- bookfarm/src/Main.java
+++ bookfarm/src/Main.java
@@ -7,17 +7,18 @@ public class Main {
             String id, pw;
         }
         class Book {
-            String title, author, sellerId;
+            String title, author;
             int price;
-        }
 
+        }
         Scanner sc = new Scanner(System.in);
-        ArrayList<User> ArrUser = new ArrayList<>();
-        ArrayList<Book> ArrBook = new ArrayList<>();
-        User LoginUser = null;
+        ArrayList<User> users = new ArrayList<>();
+        ArrayList<Book> books = new ArrayList<>();
+        User currentUser = null;
 
         while (true) {
-            if (LoginUser == null) {
+
+            if (currentUser == null) {
                 System.out.println("\n1. 회원가입 2. 로그인 0. 종료");
                 int sel = sc.nextInt();
                 sc.nextLine();
@@ -29,29 +30,42 @@ public class Main {
                     User u = new User();
                     u.id = id;
                     u.pw = pw;
-                    ArrUser.add(u);
+                    users.add(u);
                     System.out.println("회원가입 완료!");
                 } else if (sel == 2) {
                     System.out.print("아이디: ");
                     String id = sc.nextLine();
                     System.out.print("비밀번호: ");
                     String pw = sc.nextLine();
-                    boolean test = false;
-                    for (User u : ArrUser) {
-                        if ((u.id.equals(id) && u.pw.equals(pw))) {
-                            LoginUser = u;
-                            test = true;
+                    boolean found = false;
+                    for (User u : users) {
+                        if (u.id.equals(id) && u.pw.equals(pw)) {
+                            currentUser = u;
+                            found = true;
                             break;
                         }
                     }
-                    if (test) System.out.println("로그인 성공!");
+                    if (found) System.out.println("로그인 성공!");
                     else System.out.println("로그인 실패!");
-
                 } else if (sel == 0) {
                     System.out.println("프로그램 종료");
                     break;
+                }
+            } else {
+                System.out.println("\n1. 교재 등록 2. 교재 목록 보기 3. 로그아웃");
+                int sel = sc.nextInt();
+                sc.nextLine();
+                if (sel == 1) {
+                    Book b = new Book();
+                    System.out.print("책 제목: ");
+                    b.title = sc.nextLine();
+                    System.out.print("저자: ");
+                    b.author = sc.nextLine();
+                    System.out.print("가격: ");
+                    b.price = sc.nextInt();
                 }
             }
         }
     }
 }
+
